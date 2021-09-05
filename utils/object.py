8280c1112e19c83d997d754fe7bfff93a12b48a8
KalenWillits <KalenWillits@gmail.com>
2021-09-05 13:08:42 -0400

    Created the Objet model.

---
+++ utils/object.py
@@ -0,0 +1,9 @@
+class Object:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __getitem__(self, key):
+        return self.__dict__.get(key)
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
