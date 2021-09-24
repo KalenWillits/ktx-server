6451effcd50a73e07f11eff3e6db4e9a00b02174
KalenWillits <KalenWillits@gmail.com>
2021-09-24 14:29:53 -0400

    Adding automatic salt generation.

--- utils/encrypt.py
+++ utils/encrypt.py
@@ -1,6 +1,13 @@
 import hashlib
+from .generate_salt import generate_salt
 
 
-def encrypt(string, hash_salt=""):
-    derived_key = hashlib.pbkdf2_hmac('sha256', bytes(string, 'UTF-8'), hash_salt, 100_000)
+def encrypt(string):
+    """
+    Hashes a string for use in storing password to a database.
+    Optional salt string for added security.
+    """
+    salt = generate_salt()
+    derived_key = hashlib.pbkdf2_hmac('sha256', bytes(string, 'UTF-8'), bytes(salt, 'UTF-8'), 100_000)
+
     return derived_key.hex()
--- utils/generate_salt.py
+++ utils/generate_salt.py
@@ -1,9 +1,16 @@
 import os
 import secrets
 
-def generate_salt():
+def generate_salt() -> str:
     '''Checks if a salt hash has been made, if not a .salt file is created.'''
     salt_file = os.path.join(".salt")
     if not os.path.isfile(salt_file):
-        with open(salt_file, 'w+') as file:
-            file.write(secrets.token_hex())
+        with open(salt_file, "w") as file:
+            salt = secrets.token_hex()
+            file.write(salt)
+
+    else:
+        with open(salt_file, "r") as file:
+            salt = file.read()
+
+    return salt
