2f542e3d35d4d5b2cd918f01b168d09752e4b141
KalenWillits <KalenWillits@gmail.com>
2021-09-27 09:43:00 -0400

    Adding trusted list to websocket origins to server.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+
 import websockets
 import argparse
 
@@ -11,13 +12,14 @@ from .utils import get_snapshot
 class Server:
     def __init__(
         self,
-        address: str = "localhost", port: int = 5000, debug: bool = True, db=None,
+        host: str = "localhost", port: int = 5000, debug: bool = True, db=None,
         tasks=None,
         models=None,
         actions=None,
-        auth=lambda token: True,
+        trust: list = ["*"],
+        handle_auth=lambda token: True,
     ):
-        self.address = address
+        self.host = host
         self.port = port
         self.debug = debug
         self.clients = dict()
@@ -25,7 +27,8 @@ class Server:
         self.tasks = tasks
         self.models = models
         self.actions = actions
-        self.auth = auth
+        self.handle_auth = handle_auth
+        self.trust = trust
         self.commands = {
             "run": self.run_default(),
             "dev": self.run_dev(),
@@ -37,7 +40,7 @@ class Server:
             print(*args)
 
     def run_default(self):
-        return lambda: websockets.serve(self.handle, self.address, self.port)
+        return lambda: websockets.serve(self.handle, self.host, self.port)
 
     def run_dev(self):
         self.log("[DEVELOPMENT MODE]")
@@ -65,8 +68,15 @@ class Server:
 
         return lambda: embed()
 
+    def check_if_trusted(self, websocket) -> bool:
+        if websocket.remote_address[0] in self.trust or "*" in self.trust:
+            return True
+
+        self.log(f"[UNTRUSTED-SOURCE-DENIED] {websocket.remote_address}")
+        return False
+
     def state_event(self, websocket):
-        self.log(f"[NOTIFY EVENT] {websocket.host} -- {websocket.local_address}")
+        self.log(f"[NOTIFY EVENT] {websocket.remote_address}")
         snapshot = get_snapshot(self.clients[websocket], self.db)
         return json.dumps(snapshot)
 
@@ -78,45 +88,46 @@ class Server:
                 await client.send(payload)
 
     async def register(self, websocket):
-        self.log(f"[REGISTER] {websocket.host} -- {websocket.local_address}")
+        self.log(f"[REGISTER-NEW-CONNECTION] {websocket.remote_address}")
         self.clients[websocket] = dict()
 
     async def unregister(self, websocket):
-        self.log(f"[UNREGISTER] {websocket.host} -- {websocket.local_address}")
+        self.log(f"[UNREGISTER-CLOSE-CONNECTION] {websocket.remote_address}")
         del self.clients[websocket]
 
-    async def handle(self, websocket, address):
-        await self.register(websocket)
-        try:
-            await websocket.send(self.state_event(websocket))
-            async for payload in websocket:
-                data = json.loads(payload)
-                for action_name in data.keys():
-                    if response := self.actions[action_name].execute(
-                        websocket=websocket,
-                        server=self,
-                        db=self.db,
-                        auth=self.auth,
-                        **data.get(action_name)
-                    ):
-                        await self.notify_state(response)
+    async def handle(self, websocket, host):
+        if self.check_if_trusted(websocket):
+            await self.register(websocket)
+            try:
+                await websocket.send(self.state_event(websocket))
+                async for payload in websocket:
+                    data = json.loads(payload)
+                    for action_name in data.keys():
+                        if response := self.actions[action_name].execute(
+                            websocket=websocket,
+                            server=self,
+                            db=self.db,
+                            auth=self.auth,
+                            **data.get(action_name)
+                        ):
+                            await self.notify_state(response)
 
-        except ConnectionClosedError:
-            self.log(f"[CONNECTION CLOSED] {websocket.host} -- {websocket.local_address}")
+            except ConnectionClosedError:
+                self.log(f"[CONNECTION CLOSED] {websocket.remote_address}")
 
-        finally:
-            await self.unregister(websocket)
+            finally:
+                await self.unregister(websocket)
 
     def run(self):
 
         parser = argparse.ArgumentParser()
         parser.add_argument('cmd')
-        parser.add_argument('-p', '--port', default=self.port)
-        parser.add_argument('-a', '--address', default=self.address)
-        parser.add_argument('-d', '--debug', default=self.debug, action='store_false')
+        parser.add_argument('--port', default=self.port)
+        parser.add_argument('--host', default=self.host)
+        parser.add_argument('--debug', default=self.debug, action='store_false')
         args = parser.parse_args()
         self.port = args.port
-        self.address = args.address
+        self.host = args.host
         self.debug = args.debug
 
         if args.cmd == 'shell':
@@ -124,7 +135,7 @@ class Server:
 
         elif init_function := self.commands.get(args.cmd):
             try:
-                self.log(f"[STARTING] {self.address}:{self.port}")
+                self.log(f"[STARTING] {self.host}:{self.port}")
                 self.db.load()
                 self.tasks.execute_startup_tasks(db=self.db, models=self.models, server=self)
 
